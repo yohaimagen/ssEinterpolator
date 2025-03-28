a49b45c10e4396289a2654bdc310378e2be8e5e9
yohaimagen <mayochay@gmail.com>
2025-03-28 22:52:32 +0000

    moving to multi params

--- src/ssEinterpolator/data.py
+++ src/ssEinterpolator/data.py
@@ -1,29 +1,23 @@
 import numpy as np
 
 class Data:
-    def __init__(self, dir=None, sr=None, slip=None, state=None, t=None, prefix='', supix = ''):
-        if dir is None and sr is None and slip is None and state is None and t is None:
-            raise ValueError('Either dir or sr, slip, state, t must be provided')
-        if dir is None:
+    def __init__(self, params, load_f=None, str_params=None, sr=None, slip=None, state=None, t=None, prefix='', supix = ''):
+        if load_f is None and params is None and sr is None and slip is None and state is None and t is None:
+            raise ValueError('Either load_f, params,  or sr, slip, state, t must be provided')
+        if load_f is None and str_params is None:
             if sr is None or slip is None or state is None or t is None:
-                raise ValueError('if dir not provided sr, slip, state, t must be provided')
+                raise ValueError('if load_f, str_params not provided sr, slip, state, t must be provided')
             self.sr = sr
             self.slip = slip
             self.state = state
             self.t = t / (365*24*60*60)
         else:
-            def load_data(file):
-                # print(f'loading {file}')
-                data = np.load(file)
-                print(f'loaded {file} of shape {data.shape}')
-                return np.load(file)
-            self.sr = load_data(f'{dir}/{prefix}sr{supix}.npy')
-            self.state = load_data(f'{dir}/{prefix}state{supix}.npy')
-            self.slip = load_data(f'{dir}/{prefix}slip{supix}.npy')
-            self.t = load_data(f'{dir}/{prefix}t{supix}.npy') / (365*24*60*60)
+            self.sr, self.slip, self.state, self.t = load_f(str_params)
+            self.t /= (365*24*60*60)
             self.t -= self.t[0]
+        self.params = params
     
     def mask_data(self, tl, tr):
         mask = (self.t >= tl) & (self.t < tr)
-        masked_data = Data(None, sr=np.copy(self.sr[:, mask]), slip=np.copy(self.slip[:, mask]), state=np.copy(self.state[:, mask]), t=np.copy(self.t[mask]) * (365*24*60*60))
+        masked_data = Data(params=self.params, load_f=None, str_params=None, sr=np.copy(self.sr[:, mask]), slip=np.copy(self.slip[:, mask]), state=np.copy(self.state[:, mask]), t=np.copy(self.t[mask]) * (365*24*60*60))
         return masked_data
--- src/ssEinterpolator/rom.py
+++ src/ssEinterpolator/rom.py
@@ -1,13 +1,50 @@
 import numpy as np
 from scipy.interpolate import RBFInterpolator
+from ray.util.multiprocessing import Pool
 from .interpolation import *
 from .data import Data
 from .utils import find_slip_events
 
+def process_data(args):
+    print(args)
+    self, f_param, str_param = args  # Include 'self' to access instance variables
+    print(f'processing {str_param}')
+    key = '_'.join(str_param)
+    data = Data(load_f=self.load_f, str_params=str_param, params=f_param)
+    data = data.mask_data(self.t_start, self.t_end)
+    print(f'loading data for key={key} with shape {data.sr.shape}')
+    idx = np.argmin(np.abs(self.lf - self.along_dp_sses_depth_detector))
+    sses = find_slip_events(data.t, np.log10(np.abs(data.sr[idx])), threshold=self.sses_detector_threshold)
+    print(f'found {sses.shape[0]} sses for key={key}')
+    if sses.shape[0] < self.sses_starts + self.sses_num:
+        raise ValueError(f'Not enough slip events detected for key={key}')
+    split_data = self.split_data_by_sse(data, sses)
+    split_data[self.sses_starts: self.sses_starts + self.sses_num + 1]
+    # data = data.mask_data(split_data[self.sses_starts].t.min(), split_data[:self.sses_starts + self.sses_num][-1].t.max())
+    return key, split_data
+
+def calc_latent_helper(args):
+    self, idx = args
+    print(f'creating latent matrix for sse {idx}')
+    # latent = self.create_one_sse_latent_matrix(idx)
+    latent = 1
+    return latent
+
 class ROM:
-    def __init__(self, prefixes, numerals, dir, lf_path, sses_num, t_to_u_knot_l, u_to_par_knot_l, supix = '', along_dp_sses_depth_detector = 195, sses_detector_threshold=-4, sses_starts=0, t_start=0, t_end=1e99, base_step_t_interpolate=1e-4, depths_t_interpolate=9):
+    def __init__(self, f_params, str_params, load_f, lf_path, sses_num, t_to_u_knot_l, u_to_par_knot_l, along_dp_sses_depth_detector = 195, sses_detector_threshold=-4, sses_starts=0, t_start=0, t_end=1e99, base_step_t_interpolate=1e-4, depths_t_interpolate=9):
         self.lf = np.load(lf_path)
+        self.f_params = f_params
+        self.str_params = str_params
+        self.load_f = load_f
+        self.t_start = t_start
+        self.t_end = t_end
+        self.along_dp_sses_depth_detector = along_dp_sses_depth_detector
+        self.sses_detector_threshold = sses_detector_threshold
+        self.sses_starts = sses_starts
+        self.base_step_t_interpolate = base_step_t_interpolate
+        self.depths_t_interpolate = depths_t_interpolate
         self.D = {}
+        self.D_sses = {}
         self.sses_num = sses_num
         self.t_to_u_knot_l = t_to_u_knot_l
         self.u_to_par_knot_l = u_to_par_knot_l
@@ -15,11 +52,27 @@ class ROM:
         self.u_to_par_cof_l = self.u_to_par_knot_l - 4
         self.dp_laten_vec_length = self.t_to_u_knot_l + self.t_to_u_cof_l + self.u_to_par_knot_l +self. u_to_par_cof_l * 2 + 4
         self.t_interpolate = self.create_interpolation_time(base_step=base_step_t_interpolate, depths=depths_t_interpolate)
-        self.numerals = np.array(numerals).reshape(-1, 1)
-        for prefix in prefixes:
-            data = Data(dir=dir, supix=supix, prefix=prefix)
-            self.D[prefix] = data.mask_data(t_start, t_end)
-        self.sample_data_by_sses(sses_num, sses_starts, along_dp_sses_depth_detector, sses_detector_threshold)
+        # for f_param, str_param in zip(f_params, str_params):
+        #     key = '_'.join(str_param)
+        #     data = Data(load_f=load_f, str_params=str_param, params=f_param)
+        #     data = data.mask_data(t_start, t_end)
+        #     print(f'loading data for key={key} with shape {data.sr.shape}')
+        #     idx = np.argmin(np.abs(self.lf - along_dp_sses_depth_detector))
+        #     sses = find_slip_events(data.t, np.log10(np.abs(data.sr[idx])), threshold=sses_detector_threshold)
+        #     if sses.shape[0] < sses_starts + sses_num:
+        #         raise ValueError(f'Not enough slip events detected for key={key}')
+        #     split_data = self.split_data_by_sse(data, sses)
+        #     self.D_sses[key] = split_data[sses_starts: sses_starts + sses_num + 1]
+        #     self.D[key] = data.mask_data(split_data[sses_starts].t.min(), split_data[:sses_starts + sses_num][-1].t.max())
+        
+        # print(self.str_params.shape)
+        # print(str_params.shape)
+        with Pool(processes=15) as pool:
+            results = pool.map(process_data, [(self, self.f_params[k], self.str_params[k]) for k in range(len(self.f_params))])
+
+        for key, split_data in results:
+            self.D_sses[key] = split_data
+            # self.D[key] = data
         
     
 
@@ -29,24 +82,10 @@ class ROM:
             split_data.append(data.mask_data(sses[i], sses[i + 1]))
         return split_data
     
-    def sample_data_by_sses(self, sses_num, sses_starts, along_dp_sses_depth_detector=195, sses_detector_threshold=-4):
-        self.D_sses = {}
-        self.Dm = {}
-        self.sses_num = sses_num
-        for w in self.D:
-            data = self.D[w]
-            print(data.sr.shape)
-            idx = np.argmin(np.abs(self.lf - along_dp_sses_depth_detector))
-            sses = find_slip_events(data.t, np.log10(np.abs(data.sr[idx])), threshold=sses_detector_threshold)
-            if sses.shape[0] < sses_starts + sses_num:
-                raise ValueError(f'Not enough slip events detected for w={w}')
-            split_data = self.split_data_by_sse(data, sses)
-            self.D_sses[w] = split_data[sses_starts: sses_starts + sses_num + 1]
-            self.Dm[w] = data.mask_data(split_data[sses_starts].t.min(), split_data[:sses_starts + sses_num][-1].t.max())
-    
     def create_one_sse_latent_matrix(self, idx):
         if idx >= self.sses_num or idx < 0:
             raise ValueError('idx should be in range of sses_num')
+        print(f'creating latent matrix for sse {idx}')
         latent = []
         for k in self.D_sses.keys():
             data = self.D_sses[k][idx]
@@ -56,10 +95,15 @@ class ROM:
         latent = np.stack(latent)
         return latent
     
+    # def build_latent_matrices(self):
+    #     self.latent = []
+    #     for i in range(self.sses_num):
+    #         self.latent.append(self.create_one_sse_latent_matrix(i))
+    
     def build_latent_matrices(self):
-        self.latent = []
-        for i in range(self.sses_num):
-            self.latent.append(self.create_one_sse_latent_matrix(i))
+        with Pool(processes=15) as pool:
+            # Use pool.map to parallelize the creation of latent matrices
+            self.latent = pool.map(self.create_one_sse_latent_matrix, [(self, idx) for idx in range(self.sses_num)])
             
     def save_latent_matrices(self, path):
         for k, latent in enumerate(self.latent):
@@ -106,7 +150,7 @@ class ROM:
     def build_rom(self):
         self.RBFs = []
         for a in self.A:
-            rbf = RBFInterpolator(self.numerals, a, kernel='linear')
+            rbf = RBFInterpolator(self.f_params, a, kernel='linear')
             self.RBFs.append(rbf)
     
     def predict(self, w):
@@ -124,8 +168,8 @@ class ROM:
         for latent, a, u, s, vh in zip(self.latent, self.A, self.U, self.S, self.V):
             a_test = a[leave_one_out]
             a_train = np.delete(a, leave_one_out, axis=0)
-            y_test = self.numerals[leave_one_out]
-            y_train = np.delete(self.numerals, leave_one_out, axis=0)
+            y_test = self.f_params[leave_one_out]
+            y_train = np.delete(self.f_params, leave_one_out, axis=0)
             rbf = RBFInterpolator(y_train, a_train, kernel='linear')
             apred = rbf(y_test.reshape(1, -1))
             ypred = (u @ apred.T).reshape(1, -1)
@@ -133,16 +177,6 @@ class ROM:
             recostracted_sses.append(Data(sr=reconstructed_sr, state=reconstructed_state, slip=reconstructed_slip, t=t_interp * (365*24*60*60)))
         return recostracted_sses
     
-    # def build_reconstructed_time_series(self, reconstructed_sses):
-    #     sr = [reconstructed_sse.sr for reconstructed_sse in reconstructed_sses]
-    #     state = [reconstructed_sse.state for reconstructed_sse in reconstructed_sses]
-    #     slip = [reconstructed_sse.slip for reconstructed_sse in reconstructed_sses]
-    #     t = [reconstructed_sse.t for reconstructed_sse in reconstructed_sses]
-    #     sr = np.concatenate(sr, axis=1)
-    #     state = np.concatenate(state, axis=1)
-    #     slip = np.concatenate(slip, axis=1)
-    #     t = np.concatenate(t)
-    #     return Data(sr=sr, state=state, slip=slip, t=t * (365*24*60*60))
     
     def build_reconstructed_time_series(self, reconstructed_sses):
         sr = np.copy(reconstructed_sses[0].sr)
